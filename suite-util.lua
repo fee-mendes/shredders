5b6f6c9da57e1c8be0160e280560ae754218eafc
Felipe Mendes <felipemendes@scylladb.com>
2024-03-25 23:59:28 +0000

    Update constants and remove boilerplate we don't use

--- suite-util.lua
+++ suite-util.lua
@@ -1,16 +1,6 @@
--- TODO: get homedir from os.getenv?
-local BASEDIR <const> = "/home/buildbot/test/conf"
-
 -- FIXME: should load these during config stage via callouts to dig.
 -- they don't change in practice though, so low priority.
-local NODE_IPS <const> = { '10.191.24.56', '10.191.24.4', '10.191.24.178' }
-
-function nodectrl(a)
-    local result, term, code = os.execute(BASEDIR .. "/node-ctrl.sh " .. a)
-    if result == nil then
-        plog("LOG", "ERROR", "node control:" .. term .. "_" .. code)
-    end
-end
+local NODE_IPS <const> = { '172.31.19.254' }
 
 function nodeips()
     return NODE_IPS
